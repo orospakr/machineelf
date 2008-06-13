6445e46a27b8715edd685971afef3f9b46c32dea
Andrew Clunis <andrew@orospakr.ca>
2008-06-12 20:29:36 -0400

    Generated Player controller.

---
+++ machineelf2/spec/controllers/player_controller_spec.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe PlayerController do
+
+  #Delete this example and add some real ones
+  it "should use PlayerController" do
+    controller.should be_an_instance_of(PlayerController)
+  end
+
+end
