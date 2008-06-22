12ebb6cb1449b44fc7283e50606691a88f5e1300
Andrew Clunis <andrew@orospakr.ca>
2008-06-22 16:54:20 -0400

    Tossed all random files into catch-all directory.

---
+++ random_crap/munchkin.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name          Munchkin - Machine Elf's Lil Buddy.
+// @namespace     http://knightsbridge.orospakr.ca/
+// @description   Machine Elf client-side scraper for subjects of The Knightsbridge Pact.
+// @include       http://s3.ikariam.org/*
+// ==/UserScript==
+
+GM_log('wtg');
+
+alert('LOL, Intffernet.');
---
+++ random_crap/munchkin_lol.user.js
@@ -0,0 +1,1 @@
+/home/orospakr/.mozilla/firefox/345bs1j3.default/gm_scripts/munchkinmachineelfslilbu.user.js
