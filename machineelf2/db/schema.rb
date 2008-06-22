c2349ae64623f77bc42b1a3c870cf5b0de9297c9
Andrew Clunis <andrew@orospakr.ca>
2008-06-22 14:21:37 -0400

    Installed (mostly, it had a down external, I think) acts_as_state_machine plugin, and made Town index view work.

--- machineelf2/db/schema.rb
+++ machineelf2/db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 5) do
+ActiveRecord::Schema.define(:version => 6) do
 
   create_table "islands", :force => true do |t|
     t.integer  "ikariam_id"
@@ -68,6 +68,21 @@ ActiveRecord::Schema.define(:version => 5) do
     t.integer  "owner_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "login"
+    t.string   "email"
+    t.string   "crypted_password",          :limit => 40
+    t.string   "salt",                      :limit => 40
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "remember_token"
+    t.datetime "remember_token_expires_at"
+    t.string   "activation_code",           :limit => 40
+    t.datetime "activated_at"
+    t.string   "state",                                   :default => "passive"
+    t.datetime "deleted_at"
   end
 
 end
