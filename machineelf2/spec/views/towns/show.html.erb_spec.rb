c81109f65fe8dac135a1b048ed99ce4bd28b9a2a
Andrew Clunis <andrew@orospakr.ca>
2008-06-22 16:50:47 -0400

    Fixed Town show view.

--- machineelf2/spec/views/towns/show.html.erb_spec.rb
+++ machineelf2/spec/views/towns/show.html.erb_spec.rb
@@ -2,20 +2,13 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe "/towns/show.html.erb" do
   include TownsHelper
-  
+
   before(:each) do
     @town = mock_model(Town)
     @town.stub!(:island_id).and_return("1")
-    @town.stub!(:found_at).and_return(Time.now)
     @town.stub!(:updated_at).and_return(Time.now)
     @town.stub!(:name).and_return("MyString")
     @town.stub!(:owner_id).and_return("1")
-    @town.stub!(:wood).and_return("1")
-    @town.stub!(:wine).and_return("1")
-    @town.stub!(:marble).and_return("1")
-    @town.stub!(:crystal).and_return("1")
-    @town.stub!(:sulphur).and_return("1")
-    @town.stub!(:version).and_return("1")
 
     assigns[:town] = @town
   end
