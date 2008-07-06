246dc618f3359bd7afb59d48df72afe03496e264
Andrew Clunis <andrew@orospakr.ca>
2008-07-05 22:05:00 -0400

    Player now has special method for finding by ikariam login name.

--- machineelf2/app/models/player.rb
+++ machineelf2/app/models/player.rb
@@ -9,4 +9,15 @@ class Player < ActiveRecord::Base
   belongs_to :server
 
   acts_as_ikariam
+
+  def self.by_ikariam_login(ikariam_login)
+    already_exists = self.find(:first, :conditions => ["ikariam_login = ?", ikariam_login])
+    if already_exists.nil?
+      new_town = self.new
+      new_town.ikariam_id = ikariam_id
+      return new_town
+    else
+      return already_exists
+    end
+  end
 end
--- machineelf2/spec/models/player_spec.rb
+++ machineelf2/spec/models/player_spec.rb
@@ -18,8 +18,14 @@ describe Player do
     @player.should_not be_valid
   end
 
-  it "should return a new town if asked for one with a new Ikariam player id" do
+  it "should return a new player if asked for one with a new Ikariam player id" do
     dude = Player.by_ikariam_id(players(:orospakr).ikariam_id)
+    dude.server = @server
+    dude.id.should == players(:orospakr).id
+  end
+
+  it "should return an existing player if asked for one by login name" do
+    dude = Player.by_ikariam_login(players(:orospakr).ikariam_login)
     dude.server = @server
     dude.id.should == players(:orospakr).id
   end
