ebf8dbc40c9d39e9a55993ccd3f3220ee91aa8e0
Andrew Clunis <andrew@orospakr.ca>
2008-07-11 03:12:44 -0400

    install.rdf now processes with version number.

--- machineelf@knightsbridge.orospakr.ca/spin_extension.sh
+++ machineelf@knightsbridge.orospakr.ca/spin_extension.sh
@@ -1,5 +1,7 @@
 XPI_FILENAME="../machineelf2/public/toolbars/machineelf-0.1-r`bzr revno`.xpi"
+sed s/REVNO/`bzr revno`/ install.rdf.in > install.rdf
 zip -r ../machineelf2/public/toolbars/machineelf-0.1-r`bzr revno`.xpi *
 bzr add $XPI_FILENAME
+
 echo "Version is: `bzr revno`"
 echo `bzr revno` > ../machineelf2/public/toolbars/CURRENT_TOOLBAR_REVNO
