0262dc4dd6c21f30d3a85149c74991ba48be1b3c
Andrew Clunis <andrew@orospakr.ca>
2008-07-11 02:49:24 -0400

    Oops, forgot to spin new version of Toolbar.

--- machineelf@knightsbridge.orospakr.ca/spin_extension.sh
+++ machineelf@knightsbridge.orospakr.ca/spin_extension.sh
@@ -1,3 +1,5 @@
+XPI_FILENAME="../machineelf2/public/toolbars/machineelf-0.1-r`bzr revno`.xpi"
 zip -r ../machineelf2/public/toolbars/machineelf-0.1-r`bzr revno`.xpi *
+bzr add $XPI_FILENAME
 echo "Version is: `bzr revno`"
 echo `bzr revno` > ../machineelf2/public/toolbars/CURRENT_TOOLBAR_REVNO
