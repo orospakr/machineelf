c95d3b716af595a685764e3143b46ccc01bed54a
Andrew Clunis <andrew@orospakr.ca>
2008-07-11 03:46:08 -0400

    Fixed green login screen, and a few broken tests.

--- machineelf2/spec/views/players/index.html.erb_spec.rb
+++ machineelf2/spec/views/players/index.html.erb_spec.rb
@@ -2,12 +2,18 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe "/players/index.html.erb" do
   include PlayersHelper
-  
+
   before(:each) do
     player_98 = mock_model(Player)
+    player_98s_server = mock_model(Server)
+    player_98s_server.should_receive(:name).and_return("MyString")
+    player_98.should_receive(:server).twice.and_return(player_98s_server)
     player_98.should_receive(:ikariam_id).and_return("1")
     player_98.should_receive(:ikariam_login).and_return("MyString")
     player_99 = mock_model(Player)
+    player_99s_server = mock_model(Server)
+    player_99s_server.should_receive(:name).and_return("MyString")
+    player_99.should_receive(:server).twice.and_return(player_99s_server)
     player_99.should_receive(:ikariam_id).and_return("1")
     player_99.should_receive(:ikariam_login).and_return("MyString")
 
