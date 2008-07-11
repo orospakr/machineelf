1aa7227870c8c14714676a47bb7cc5bb68216a23
Andrew Clunis <andrew@orospakr.ca>
2008-07-11 02:39:04 -0400

    Improvements to layout and CSS.

--- machineelf2/app/helpers/site_helper.rb
+++ machineelf2/app/helpers/site_helper.rb
@@ -1,2 +1,7 @@
 module SiteHelper
+  def get_bzr_revno
+    f = File.new(RAILS_ROOT + "/public/toolbars/CURRENT_TOOLBAR_REVNO", "r")
+    revno = f.read.strip
+  end
+
 end
--- machineelf@knightsbridge.orospakr.ca/spin_extension.sh
+++ machineelf@knightsbridge.orospakr.ca/spin_extension.sh
@@ -1,1 +1,3 @@
 zip -r ../machineelf2/public/toolbars/machineelf-0.1-r`bzr revno`.xpi *
+echo "Version is: `bzr revno`"
+echo `bzr revno` > ../machineelf2/public/toolbars/CURRENT_TOOLBAR_REVNO
