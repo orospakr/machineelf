8787497ad5f65a32a4fae8738899d9a153252cd8
Andrew Clunis <andrew@orospakr.ca>
2008-07-12 02:12:20 -0400

    Made version numbers of toolbar XPIs much cleverer.

--- machineelf@knightsbridge.orospakr.ca/spin_extension.sh
+++ machineelf@knightsbridge.orospakr.ca/spin_extension.sh
@@ -1,7 +1,10 @@
-XPI_FILENAME="../machineelf2/public/toolbars/machineelf-0.1-r`bzr revno`.xpi"
-sed s/REVNO/`bzr revno`/ install.rdf.in > install.rdf
-zip -r ../machineelf2/public/toolbars/machineelf-0.1-r`bzr revno`.xpi *
+REVNO=`bzr revno`
+VERSION=$(( $REVNO + 1 ))
+XPI_FILENAME="../machineelf2/public/toolbars/machineelf-0.1-r$VERSION.xpi"
+sed s/REVNO/$VERSION/ install.rdf.in > install.rdf
+zip -r $XPI_FILENAME *
 bzr add $XPI_FILENAME
 
-echo "Version is: `bzr revno`"
-echo `bzr revno` > ../machineelf2/public/toolbars/CURRENT_TOOLBAR_REVNO
+echo "Version is: $VERSION.  You should do a commit now so that the revision"
+echo "number of the XPI actually reflects of the contents of that commit id."
+echo $VERSION > ../machineelf2/public/toolbars/CURRENT_TOOLBAR_REVNO
